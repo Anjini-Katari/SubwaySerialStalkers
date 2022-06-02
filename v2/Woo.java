92e14ffd0d56390472587deb4955250f608c096c
Hamim Seam <hseam30@stuy.edu>
2022-06-02 13:29:08 -0400

    update

--- v2/Woo.java
+++ v2/Woo.java
@@ -8,31 +8,35 @@ public class Woo {
         Train train = new Train(); //constructor of Train class
         Protagonist mc = new Protagonist(); //constructor of main character
 
-        System.out.print("Hey there. You ready to play?: ");
+        System.out.print("Hey there. You ready to play? (Type 'yes' to confirm): ");
 
-        try {
-          if (s.nextLine().toLowerCase() == "yes") {
-            System.out.println("You better be. Entering game...");
+        if (sc.nextLine().toLowerCase().equals("yes")) {
+          System.out.println("You better be. Entering game...");
+          System.out.println("Train name?");
 
-            //animation of entering game
-            int l = 0;
-            while (l < 25) {
-              System.out.print(" - ");
-              Thread.sleep(100);
-              l++;
-            } //end animation
+          //animation of entering game
+          // int l = 0;
+          // while (l < 25) {
+          //   System.out.print(" - ");
+          //   //Thread.sleep(100)
+          //   l++;
+          // } //end animation
 
-            String mcName = mc.setName(sc.nextLine());
-            System.out.println("Okay " + mcName);
-            System.out.println("Train:\n" + train);
 
-            sc.close();
-          }
-        } //end try
+          String mcName = mc.setName(sc.nextLine());
+          System.out.println("Okay " + mcName);
+          System.out.println("Train:\n" + train);
 
-        catch(Exception e) {
-          System.out.println("Um...You suck. Exiting game...");
-          System.exit();
-        } //end catch
+        }
+        else {
+          System.exit(0);
+        }
+
+        System.out.println("You're riding the 2 train, on your way to Stuyvesant High School.");
+        System.out.println("All of a sudden, the lights flicker, before the train falls into complete darkness...");
+        System.out.println("The passengers all fall into chaos!");        
+
+        sc.close();
+
     }
 }
